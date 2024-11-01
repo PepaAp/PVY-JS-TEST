512c05ce6f11e79899caf9141b0e6992b62fb18c
Josef Aron <144839725+PepaAp@users.noreply.github.com>
2024-11-01 10:30:42 +0000

    update button

--- dice/main.js
+++ dice/main.js
@@ -4,28 +4,44 @@ const statistika = document.getElementById('statistika');
 const btnHrej = document.querySelector('.btn.btn-danger'); 
 let hod;
 let pocetHodu = [];
+let clickFlag = false;
+let intervalId;
 
 btnHrej.addEventListener('click', function() {
-    console.log('hraje to');
-    hod = Math.ceil(Math.random() * 6);
-    captionKostka.innerHTML = `<span class = "badge rounded-pill bg-danger">${hod}</span>`;
-    imgKostka.src = `img/kostka${hod}.png`;
-    pocetHodu.push(hod);
-    stats();
-    console.log(sumaHodu())
+    if (clickFlag == false) {
+        animate();
+        clickFlag = true;
+        btnHrej.textContent = "Stop!"
+    } else {
+        clearInterval(intervalId);
+        hod = Math.ceil(Math.random() * 6);
+        captionKostka.innerHTML = `<span class = "badge rounded-pill bg-danger">${hod}</span>`;
+        imgKostka.src = `img/kostka${hod}.png`;
+        pocetHodu.push(hod);
+        stats();
+        console.log(sumaHodu());
+        clickFlag = false;
+        btnHrej.textContent = "Hrej"
+    }
 });
 
-function sumaHodu () {
+function animate() {
+    intervalId = setInterval(() => {
+        imgKostka.src = `img/kostka${Math.ceil(Math.random() * 6)}.png`;
+    }, 300);
+}
+
+function sumaHodu() {
     let suma = 0;
-    for (let i = 0; i < pocetHodu.length; i++){
+    for (let i = 0; i < pocetHodu.length; i++) {
         suma += pocetHodu[i];
     }
     return suma;
 }
 
-function minHody(){
+function minHody() {
     let min = 6;
-    for (let i = 0; i < pocetHodu.length; i++){
+    for (let i = 0; i < pocetHodu.length; i++) {
         if (pocetHodu[i] < min) {
             min = pocetHodu[i];
         }
@@ -33,9 +49,9 @@ function minHody(){
     return min;
 }
 
-function maxHody(){
+function maxHody() {
     let max = 1;
-    for (let i = 0; i < pocetHodu.length; i++){
+    for (let i = 0; i < pocetHodu.length; i++) {
         if (pocetHodu[i] > max) {
             max = pocetHodu[i];
         }
@@ -43,7 +59,7 @@ function maxHody(){
     return max;
 }
 
-function prumHod(){
+function prumHod() {
     let prumHod = sumaHodu() / pocetHodu.length;
     prumHod = prumHod.toFixed(2);
     return prumHod;
